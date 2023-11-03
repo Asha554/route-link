058ae55c04076820571d627fc141a5fe454e10cc
Asha554 <asha.21ai008@sode-edu.in>
2023-11-03 17:11:39 +0530

    added router

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function NavBar() {
+  return (
+    <div>
+        <Link to="/home">Home</Link><br/>
+        <Link to="/about">About</Link><br/>
+        <Link to="/Contacts">Contacts</Link>
+
+    </div>
+  )
+}
+
+export default NavBar
